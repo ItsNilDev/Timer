74bd093dd84ea6425e4e38d369b5ef5a053f4ee0
Pouridev <poria2100@gmail.com>
2020-11-03 14:47:24 +0330

    Completed Functions and added timetocount

--- file1.cpp
+++ file1.cpp
@@ -5,10 +5,13 @@ int convertMinutesToSeconds(int i)
 {
     int MINUTE = 60;
     i = MINUTE * i;
+    return i;
 }
 
 int main()
 {
-
+    int timetocount;
+    std::cin >> timetocount;
+    convertMinutesToSeconds(timetocount);
 }
 

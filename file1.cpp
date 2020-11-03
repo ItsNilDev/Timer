8fe6f6b50065b5fc6801ade3889151ec1888b06f
Pouridev <poria2100@gmail.com>
2020-11-03 17:33:02 +0330

    Completed program with seconds, v1.0

--- file1.cpp
+++ file1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <unistd.h>
 
+int ONE_SECOND = 1000;
 
 int convertMinutesToSeconds(int i)
 {
@@ -10,9 +12,17 @@ int convertMinutesToSeconds(int i)
 
 int main()
 {
+
     int timetocount;
     std::cin >> timetocount;
-    convertMinutesToSeconds(timetocount);
-    //added this line
+    int seconds = convertMinutesToSeconds(timetocount);
+    for(int i = seconds; i > 0; i--)
+    {
+        std::cout << "Time Left: " << i << std::endl;
+        // MicroSeconds
+        
+        usleep(ONE_SECOND * 1000);
+
+    }
 }
 
